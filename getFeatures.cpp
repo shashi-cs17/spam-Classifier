8d9f2bba9855be77ab95aecd40cf14e558adc1c6
shashi-cs17 <46399137+shashi-cs17@users.noreply.github.com>
2019-06-24 17:17:55 +0530

    getting a feature vector
    Model description in Model.txt

--- getFeatures.cpp
+++ getFeatures.cpp
@@ -2,10 +2,21 @@
 #include<fstream>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define mp make_pair
 
+
+//
+int offset=25;
+int cntt=25;
+double prec=0.5;
+//
 map<string,bool> tmp;
 map<string,double> data;
 string s;double ps;int total=0;
+//
+ifstream in,fin,din;
+ofstream fout;
+//
 
 vector<string> tokenize(string line){
     vector<string> vtmp;
@@ -33,76 +44,104 @@ void getData(string line){
         }
     }
     ps = atof(tt.c_str());
-    //return vtmp;
 }
 
+int buildMatrix(string fname){
+
+    string header,line;
+    map<string ,int> stx;
+    set< pair<double,string> > hav;
+    set< pair<double,string> >::iterator it;
+
+    int ind = 0;
+
+    while(getline(fin,header)){
+        ind++;
+        if(ind%500 == 0)cout<<"cleared --> "<<ind<<endl;
+        if(header.length() <= 1)break;
+        if(header == "END")break;
+        in.open(("/home/trent/Dev/octave/projData/enron1/"+fname+"/"+header).c_str());
+        if(!in.is_open()){
+            cout<<"error in "<<fname<<" = "<<ind<<endl;continue;
+        }
+        //
+        stx.clear();hav.clear();int tw=0;int havtw=0;
+        while(getline(in,line)){
+            transform(line.begin(),line.end(),line.begin(),::tolower);
+            vector<string> v = tokenize(line);
+            for(string sx:v){
+                tw++;
+                if(tmp[sx])havtw++,hav.insert(mp(data[sx],sx)),stx[sx] = stx[sx]+1;
+            }
+        }
+        double rel= (double)0;int xrel=0;
+        it = hav.lower_bound(mp(prec,"a"));
+        while(it!=hav.end()){
+            xrel = xrel + stx[(*it).second];it++;
+        }
+        rel = (double)xrel+(double)offset;rel = rel/(havtw-xrel+offset);
+        // P(HAM | "word");
+            it=hav.begin();int y=0;double dy;
+            while(it!=hav.end()){
+                if(y>=cntt)break;dy = (double)1 - (*it).first;
+                dy = (double)(1+dy)*(double)(stx[(*it).second]+offset);dy = dy/(double)(tw+offset);
+                fout<<dy<<" ";y++;it++;
+            }
+            dy = (double)offset/(double)(tw+offset);
+            while(y<cntt){fout<<dy<<" ";y++;}
+            //fout<<endl;
+        // P(SPAM | "word");
+            it = hav.end();y=0;
+            while(it!=hav.begin()){
+                if(y>=cntt)break;it--;dy = (*it).first;
+                dy = (double)(1+dy)*(double)(stx[(*it).second]+offset);dy = dy/(double)(tw+offset);
+                fout<<dy*rel<<" ";y++;
+            }
+            dy = (double)offset/(double)(tw+offset);
+            while(y<cntt){fout<<dy*rel<<" ";y++;}
+            fout<<endl;
+        //
+        in.close();
+    }
+
+    return ind;
+}
 
 int main(){
     fastio;
     //
-    double as[10];
-    multiset<double> stx;
-    multiset<double>::iterator it;
+    //------------------------------------------------------------->>>>>>>>>>>>>>>>>>>
     //
-    string header,line;
-    ifstream in,fin,din;
-    ofstream fout;
+    string line;int opt=0;
     din.open("/home/trent/Dev/octave/projData/dataMap.txt");
     if(!din.is_open()){cout<<"canntopen"<<endl;return 0;}
     while(getline(din,line)){
         if(line.length()<=1)break;
         getData(line);
-        if(ps!=(double)0 && ps!=(double)1){
+        //if(ps!=(double)0 && ps!=(double)1){
             data[s]=ps;
             tmp[s]=true;
+            if(prec<=ps)opt++;
+            //prec = ps;
             total++;
-        }
+        //}
     }
+    //prec = prec/(double)total;
     din.close();
     cout<<"total = "<<total<<endl;
+    cout<<"precision = "<<opt<<endl;
+    //
+    //
+    //------------------------------------------------------------->>>>>>>>>>>>>>>>>>>
+    //
     //
     fout.open("/home/trent/Dev/octave/projData/hammat.txt");
     fin.open("/home/trent/Dev/octave/projData/hamnames.txt");
     if(fin.is_open() == false || fout.is_open() == false){
         cout<<"error"<<endl;return 0;
     }
-    int ind=0;//fout<<"ham"<<endl;
     cout<<"---------Ham mails----------"<<endl;
-    while(getline(fin,header)){
-        ind++;
-        if(ind%500 == 0)cout<<"cleared --> "<<ind<<endl;
-        if(header.length() <= 1)break;
-        if(header == "END")break;
-        in.open(("/home/trent/Dev/octave/projData/enron1/ham/"+header).c_str());
-        if(!in.is_open()){
-            cout<<"error in ham = "<<ind<<endl;continue;
-        }
-        stx.clear();
-        while(getline(in,line)){
-            transform(line.begin(),line.end(),line.begin(),::tolower);
-            vector<string> v = tokenize(line);
-            for(string sx:v){
-                if(tmp[sx])stx.insert(data[sx]);
-            }
-        }
-        // P(HAM | "word");
-            it=stx.begin();int y=0;double dy;
-            while(it!=stx.end()){
-                if(y>=10)break;dy = (double)1 - (*it);fout<<dy<<" ";y++;it++;
-            }
-            while(y<10){fout<<0<<" ";y++;}
-            //fout<<endl;
-        // P(SPAM | "word");
-            it = stx.end();y=0;
-            while(it!=stx.begin()){
-                if(y>=10)break;it--;fout<<(*it)<<" ";y++;
-            }
-            while(y<10){fout<<0<<" ";y++;}
-            fout<<endl;
-        //
-        in.close();
-    }
-    cout<<"found end at = "<<ind<<endl;
+    cout<<buildMatrix("ham")<<endl;cout<<"END"<<endl;
     fin.close();fout.close();
     //
     //
@@ -114,43 +153,8 @@ int main(){
     if(fin.is_open() == false || fout.is_open() == false){
         cout<<"error"<<endl;return 0;
     }
-    ind=0;//fout<<"spam"<<endl;
     cout<<"---------spam mails----------"<<endl;
-    while(getline(fin,header)){
-        ind++;
-        if(ind%300 == 0)cout<<"cleared --> "<<ind<<endl;
-        if(header.length() <= 1)break;
-        if(header == "END")break;
-        in.open(("/home/trent/Dev/octave/projData/enron1/spam/"+header).c_str());
-        if(!in.is_open()){
-            cout<<"error in spam = "<<ind<<endl;continue;
-        }
-        stx.clear();
-        while(getline(in,line)){
-            transform(line.begin(),line.end(),line.begin(),::tolower);
-            vector<string> v = tokenize(line);
-            for(string sx:v){
-                if(tmp[sx])stx.insert(data[sx]);
-            }
-        }
-        // P(HAM | "word");
-            it=stx.begin();int y=0;double dy;
-            while(it!=stx.end()){
-                if(y>=10)break;dy = (double)1 - (*it);fout<<dy<<" ";y++;it++;
-            }
-            while(y<10){fout<<0<<" ";y++;}
-            //fout<<endl;
-        // P(SPAM | "word");
-            it = stx.end();y=0;
-            while(it!=stx.begin()){
-                if(y>=10)break;it--;fout<<(*it)<<" ";y++;
-            }
-            while(y<10){fout<<0<<" ";y++;}
-            fout<<endl;
-        //
-        in.close();
-    }
-    cout<<"found end at = "<<ind<<endl;
+    cout<<buildMatrix("spam")<<endl;cout<<"END"<<endl;
     fin.close();fout.close();
 
     return 0;
